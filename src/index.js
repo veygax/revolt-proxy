a018d88a245d4ecdc06962639a0b13771b6e7460
VeygaX <84923630+veygax@users.noreply.github.com>
2024-09-20 08:56:39 -0700

    it works now

--- src/index.js
+++ src/index.js
@@ -1,15 +1,74 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+	  return await handleRequest(request, env);
 	},
-};
+  };
+  
+  async function parseResponseByContentType(response, contentType) {
+	if (!contentType) return await response.text();
+  
+	switch (true) {
+	  case contentType.includes('application/json'):
+		return JSON.stringify(await response.json());
+	  case contentType.includes('text/html'):
+		const transformedResponse = new HTMLRewriter()
+		  .on('body', {
+			element(element) {
+			  element.append(
+				`
+				  <style>
+				  /* Custom CSS */
+				  </style>
+				`,
+				{ html: true }
+			  );
+			  element.append(
+				`
+				  <script>
+				  // Custom JS
+				  </script>
+				`,
+				{ html: true }
+			  );
+			},
+		  })
+		  .transform(response);
+		return await transformedResponse.text();
+	  case contentType.includes('font'):
+	  case contentType.includes('image'):
+		return await response.arrayBuffer();
+	  default:
+		return await response.text();
+	}
+  }
+  
+  async function handleRequest(request, env) {
+	const path = new URL(request.url).pathname;
+	let url = 'https://bento.me' + path;
+  
+	if (path.includes('v1')) {
+	  url = 'https://api.bento.me' + path;
+	}
+  
+	if (url === 'https://bento.me/') {
+	  url = `https://bento.me/${env.BENTO_USERNAME}`;
+	}
+  
+	let headers = {
+	  'Access-Control-Allow-Origin': '*',
+	  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+	};
+  
+	const response = await fetch(url, { headers });
+	const contentType = response.headers.get('content-type');
+	let results = await parseResponseByContentType(response, contentType);
+  
+	if (!(results instanceof ArrayBuffer)) {
+	  results = results.replaceAll('https://api.bento.me', env.BASE_URL);
+	}
+  
+	headers['content-type'] = contentType;
+  
+	return new Response(results, { headers });
+  }
+  
